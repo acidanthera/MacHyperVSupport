7c384891e815a81c34d43c7a9e1a885bb35377fd
John Davis <goldfishnet@aol.com>
2022-06-27 20:57:17 -0500

    Implement high/low allocators

--- MacHyperVSupport/ModuleDevice/HyperVModuleDevice.cpp
+++ MacHyperVSupport/ModuleDevice/HyperVModuleDevice.cpp
@@ -28,31 +28,90 @@ bool HyperVModuleDevice::start(IOService *provider) {
   // Add memory ranges from ACPI.
   //
   OSData *acpiAddressSpaces = OSDynamicCast(OSData, provider->getProperty("acpi-address-spaces"));
-  if (acpiAddressSpaces != nullptr) {
-    AppleACPIRange *acpiRanges = (AppleACPIRange*) acpiAddressSpaces->getBytesNoCopy();
-    UInt32 acpiRangeCount = acpiAddressSpaces->getLength() / sizeof (AppleACPIRange);
+  if (acpiAddressSpaces == nullptr) {
+    HVSYSLOG("Unable to locate acpi-address-spaces property on VMOD device");
     
-    OSArray *deviceMemoryArray = OSArray::withCapacity(acpiRangeCount);
-    rangeAllocator = IORangeAllocator::withRange(0);
-    if (deviceMemoryArray != nullptr) {
-      for (int i = 0; i < acpiRangeCount; i++) {
-        HVDBGLOG("Range type %u, min 0x%llX, max 0x%llX, len 0x%llX", acpiRanges[i].type, acpiRanges[i].min, acpiRanges[i].max, acpiRanges[i].length);
-        
-        IODeviceMemory *deviceMemory = IODeviceMemory::withRange(acpiRanges[i].min, acpiRanges[i].length);
-        if (deviceMemory != nullptr) {
-          deviceMemoryArray->setObject(deviceMemory);
-          rangeAllocator->deallocate(acpiRanges[i].min, acpiRanges[i].length);
-          deviceMemory->release();
-        }
-      }
-      setDeviceMemory(deviceMemoryArray);
+    super::stop(provider);
+    return false;
+  }
+  
+  AppleACPIRange *acpiRanges = (AppleACPIRange*) acpiAddressSpaces->getBytesNoCopy();
+  UInt32 acpiRangeCount = acpiAddressSpaces->getLength() / sizeof (AppleACPIRange);
+  
+  OSArray *deviceMemoryArray = OSArray::withCapacity(acpiRangeCount);
+  rangeAllocatorLow = IORangeAllocator::withRange(0);
+  rangeAllocatorHigh = IORangeAllocator::withRange(0);
+  if (deviceMemoryArray == nullptr || rangeAllocatorLow == nullptr || rangeAllocatorHigh == nullptr) {
+    HVSYSLOG("Unable to allocate range allocators");
+    
+    OSSafeReleaseNULL(deviceMemoryArray);
+    OSSafeReleaseNULL(rangeAllocatorLow);
+    OSSafeReleaseNULL(rangeAllocatorHigh);
+    super::stop(provider);
+    return false;
+  }
+  
+  for (int i = 0; i < acpiRangeCount; i++) {
+    bool isHighRange = acpiRanges[i].min > UINT32_MAX;
+    
+    HVDBGLOG("Range type %u, min 0x%llX, max 0x%llX, len 0x%llX, high %u", acpiRanges[i].type, acpiRanges[i].min, acpiRanges[i].max, acpiRanges[i].length, isHighRange);
+    
+    IODeviceMemory *deviceMemory = IODeviceMemory::withRange(acpiRanges[i].min, acpiRanges[i].length);
+    if (deviceMemory == nullptr) {
+      HVSYSLOG("Unable to allocate device memory for range 0x%llX", acpiRanges[i].min);
       deviceMemoryArray->release();
+      rangeAllocatorLow->release();
+      rangeAllocatorHigh->release();
+      super::stop(provider);
+      return false;
+    }
+    
+    // Add to device memory array and appropriate range allocator.
+    deviceMemoryArray->setObject(deviceMemory);
+    deviceMemory->release();
+    if (isHighRange) {
+      rangeAllocatorHigh->deallocate(acpiRanges[i].min, acpiRanges[i].length);
+    } else {
+      rangeAllocatorLow->deallocate(acpiRanges[i].min, acpiRanges[i].length);
     }
   }
   
-  rangeAllocator->allocateRange(0x40000000, 0x4000000);
+  // Set device memory with found ranges.
+  setDeviceMemory(deviceMemoryArray);
+  deviceMemoryArray->release();
   
-  HVDBGLOG("Hyper-V Module Device initialized with free size: %u bytes", rangeAllocator->getFreeCount());
+  // Reserve FB memory
+  // TODO: make this dynamic
+  rangeAllocatorLow->allocateRange(0x40000000, 0x4000000);
+  
+  HVDBGLOG("Hyper-V Module Device initialized with free size: %u bytes (low) %u bytes (high)",
+           rangeAllocatorLow->getFreeCount(), rangeAllocatorHigh->getFreeCount());
   registerService();
   return true;
+}
+
+void HyperVModuleDevice::stop(IOService *provider) {
+  rangeAllocatorLow->release();
+  rangeAllocatorHigh->release();
+}
+
+IORangeScalar HyperVModuleDevice::allocateRange(IORangeScalar size, IORangeScalar alignment, bool highMemory) {
+  IORangeScalar range;
+  bool result = false;
+  
+  if (highMemory) {
+    result = rangeAllocatorHigh->allocate(size, &range, alignment);
+  } else {
+    result = rangeAllocatorLow->allocate(size, &range, alignment);
+  }
+  
+  return result ? range : 0;
+}
+
+void HyperVModuleDevice::freeRange(IORangeScalar start, IORangeScalar size) {
+  if (start > UINT32_MAX) {
+    rangeAllocatorHigh->deallocate(start, size);
+  } else {
+    rangeAllocatorLow->deallocate(start, size);
+  }
 }
--- MacHyperVSupport/ModuleDevice/HyperVModuleDevice.hpp
+++ MacHyperVSupport/ModuleDevice/HyperVModuleDevice.hpp
@@ -18,14 +18,18 @@ class HyperVModuleDevice : public IOService {
   
 private:
   bool debugEnabled = false;
-  
+  IORangeAllocator *rangeAllocatorLow;
+  IORangeAllocator *rangeAllocatorHigh;
   
 public:
-  IORangeAllocator *rangeAllocator;
   //
   // IOService overrides.
   //
   virtual bool start(IOService *provider) APPLE_KEXT_OVERRIDE;
+  virtual void stop(IOService *provider) APPLE_KEXT_OVERRIDE;
+  
+  IORangeScalar allocateRange(IORangeScalar size, IORangeScalar alignment, bool highMemory);
+  void freeRange(IORangeScalar start, IORangeScalar size);
 };
 
 #endif
